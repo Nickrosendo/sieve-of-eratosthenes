cde5c3df631ffa1988b4f56431bccd7fae061372
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2020-07-27 08:53:32 -0300

    wip(rust sieve): Create inner_sieve to mark multiples of 2

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,16 @@
+fn inner_sieve(n: i32) {
+    let mut sieve = 2..=n;
+    let n_sieve = sieve.filter(|n| !n % 2 == 0);
+    for i in n_sieve {
+        println!("{}", i);
+    }
+}
+
+fn siever(n: i32) {
+    inner_sieve(n);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let n: i32 = 5;
+    siever(n);
 }
