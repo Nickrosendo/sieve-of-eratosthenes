620f58d71aba1dc7ab614e2109e89b82402b98b4
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2020-07-27 07:31:40 -0300

    wip(rust) create rust folder

---
+++ rust/target/debug/deps/rust-7b1d25e46b88d987.d
@@ -0,0 +1,5 @@
+/home/nickrosendo/tech/sieve-of-eratosthenes/rust/target/debug/deps/rust-7b1d25e46b88d987: src/main.rs
+
+/home/nickrosendo/tech/sieve-of-eratosthenes/rust/target/debug/deps/rust-7b1d25e46b88d987.d: src/main.rs
+
+src/main.rs:
---
+++ rust/target/debug/rust.d
@@ -0,0 +1,1 @@
+/home/nickrosendo/tech/sieve-of-eratosthenes/rust/target/debug/rust: /home/nickrosendo/tech/sieve-of-eratosthenes/rust/src/main.rs
