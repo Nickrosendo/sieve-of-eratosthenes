3348ebe4af623177700f237e934c12044ae54fc3
Nicolas Rosendo <nicolasrosendo@hotmail.com>
2020-08-10 11:02:31 -0300

    feat(typescript): Add typescript inital implementation

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,16 +1,17 @@
-fn inner_sieve(n: i32) {
-    let mut sieve = 2..=n;
-    let n_sieve = sieve.filter(|n| !n % 2 == 0);
-    for i in n_sieve {
-        println!("{}", i);
-    }
+fn inner_sieve(p: i32, sieve: impl Iterator<Item = i32>) -> impl Iterator<Item = i32> {
+    sieve.filter(move |n| (n % p != 0 || *n == p))
 }
 
 fn siever(n: i32) {
-    inner_sieve(n);
+    let p = 2;
+    let sieve = p..=n;
+    let mut inner_sieved = inner_sieve(p, sieve);
+    for j in inner_sieve(3, &mut inner_sieved) {
+        println!("{  }", j);
+    }
 }
 
 fn main() {
-    let n: i32 = 5;
+    let n: i32 = 12;
     siever(n);
 }
---
+++ typescript/src/index.ts
@@ -0,0 +1,14 @@
+const inner_sieve = (p: number, sieve: Array<number>): Array<Number> => {
+  return sieve.filter((n: number) => n % p !== 0 || n == p);
+}
+
+const siever = (n: number) => {
+  let p = 2;
+}
+
+const main = () => {
+  console.log('hello typescript!')
+}
+
+
+main()
